fb09250e645ffdb0abf0c80c05fbd3630b6c0df9
Din <dinizkakov@gmail.com>
2024-10-25 17:55:17 +0100

    com

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build',  // Change this to 'build' instead of 'dist'
-  },
+  base: '/Portfolio-Website/',
   plugins: [react()],
+
 })
 
 
