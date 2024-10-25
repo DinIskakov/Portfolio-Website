5876765eb7ca17587f30cd740160be8c8304db0f
Din <dinizkakov@gmail.com>
2024-10-25 17:42:13 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',  // Change this to 'build' instead of 'dist'
+  },
   plugins: [react()],
 })
+
+
