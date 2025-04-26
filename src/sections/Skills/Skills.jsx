32bab04c5ecf97e6e6ee321321978c95dda8f0c3
DinIskakov <dinizkakov@gmail.com>
2025-04-26 16:53:05 +0500

    Update skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -16,11 +16,14 @@ function Skills() {
     <section id="skills" className={styles.container}>
         <h1 className='sectionTitle'>Skills</h1>
         <div className={styles.skillList}>
-            <SkillList src={checkMarkIcon} skill="HTML"></SkillList>
-            <SkillList src={checkMarkIcon} skill="CSS"></SkillList>
             <SkillList src={checkMarkIcon} skill="JavaScript"></SkillList>
             <SkillList src={checkMarkIcon} skill="Python"></SkillList>
             <SkillList src={checkMarkIcon} skill="ML"></SkillList>
+            <SkillList src={checkMarkIcon} skill="AI"></SkillList>
+            <SkillList src={checkMarkIcon} skill="HTML"></SkillList>
+            <SkillList src={checkMarkIcon} skill="CSS"></SkillList>
+            <SkillList src={checkMarkIcon} skill="SQL"></SkillList>
+
         </div>
     </section>
   )
